1d815a59938fd92f5ce98a38adf7d6b9aa955ec3
Nyhm13 <ryo131995@gmail.com>
2025-06-30 12:05:27 +0200

    Initial commit

---
+++ src/main/java/it/piscinaOrchidea/VillaOrchidea/VillaOrchideaApplication.java
@@ -0,0 +1,13 @@
+package it.piscinaOrchidea.VillaOrchidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillaOrchideaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillaOrchideaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/piscinaOrchidea/VillaOrchidea/VillaOrchideaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.piscinaOrchidea.VillaOrchidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillaOrchideaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
