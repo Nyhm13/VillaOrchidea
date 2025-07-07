71aa1ddace56a675295e274102e9cbb66d31fab5
Nyhm13 <ryo131995@gmail.com>
2025-07-07 18:24:26 +0200

    adds fix to the cors configuration

--- src/main/java/it/piscinaOrchidea/VillaOrchidea/security/SecurityConfig.java
+++ src/main/java/it/piscinaOrchidea/VillaOrchidea/security/SecurityConfig.java
@@ -45,8 +45,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
